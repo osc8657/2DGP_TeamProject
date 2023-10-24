dfcdd31008d8fad072f63523d1a4a36b4821da1c
osc8657 <osc8657@naver.com>
2023-10-24 15:45:29 +0900

    상대방 기본 틀 추가

---
+++ opponent.py
@@ -0,0 +1,23 @@
+# 이동 클래스
+class Run:
+    pass
+
+# 스윙 클래스
+class Swing:
+    pass
+
+# 점프 스매시 클래스
+class Jump_smash:
+    pass
+
+# Idle 클래스
+class Idle:
+    pass
+
+# 상태변화 클래스
+class StateMachine:
+    pass
+
+# 상대방 클래스
+class Opponent:
+    pass
