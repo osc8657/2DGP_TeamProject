b599900b20f0e5c547146dc99f7b63fc65209e66
osc8657 <osc8657@naver.com>
2023-10-24 15:41:47 +0900

    main 수정

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from pico2d import *
 
 # 조작
 def handle_event():
+    pass
 
 # 월드 생성
 def create_world():
+    pass
 
 # 월드 업데이트
 def update_world():
+    pass
 
 # 월드 그리기
 def render_world():
+    pass
 
