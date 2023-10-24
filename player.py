d4eef484cc6d92073ee9bd39bd8759c1be163251
osc8657 <osc8657@naver.com>
2023-10-24 15:41:07 +0900

    player 기본 틀 추가

---
+++ player.py
@@ -0,0 +1,47 @@
+from pico2d import *
+import game_stadium
+
+
+# 우측 키 이벤트
+def right_down(e):
+    pass
+def right_up(e):
+    pass
+
+#좌측 키 이벤트
+def left_down(e):
+    pass
+def left_up(e):
+    pass
+
+#위쪽 키 이벤트
+def up_down(e):
+    pass
+def up_up(e):
+    pass
+
+# 스페이스 키 이벤트
+def space_down(e):
+    pass
+def space_up(e):
+    pass
+
+# 이동 클래스
+class Run:
+    pass
+
+# 스윙 클래스
+class Swing:
+    pass
+
+# idle 클래스
+class Idle:
+    pass
+
+# 상태변환 클래스
+class StateMachine:
+    pass
+
+# 플레이어 클래스
+class Player:
+    pass
