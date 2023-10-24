c131487cd04dd65418ad97edab8e13b766c1b109
osc8657 <osc8657@naver.com>
2023-10-24 14:24:21 +0900

    월드 업데이트 함수 구현

--- game_stadium.py
+++ game_stadium.py
@@ -1,6 +1,15 @@
+
+
 # 게임 월드의 표현
+# 레이어로 다루도록 수정해야함
 objects = []
 
 # 월드에 객체를 넣는 함수
 def add(o):
     objects.append(o)
+
+# 월드의 객체들을 모두 업데이트하는 함수
+def update():
+    for o in objects:
+        objects.update()
+
