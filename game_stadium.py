d63d16ec2071e4e618f0681999116368fc360746
osc8657 <osc8657@naver.com>
2023-10-24 15:54:37 +0900

    함수 호출 완료시 메세지 출력

--- game_stadium.py
+++ game_stadium.py
@@ -7,13 +7,16 @@ objects = []
 # 월드에 객체를 넣는 함수
 def add(o):
     objects.append(o)
+    print('add complete')
 
 # 월드의 객체들을 모두 업데이트하는 함수
 def update():
     for o in objects:
         objects.update()
+    print('update complete')
 
 # 월드 객체들을 모두 그리는 함수
 def render():
     for o in objects:
         objects.draw()
+    print("render complete")
